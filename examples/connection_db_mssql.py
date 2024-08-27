ba359655d434797c1684b7395fab9f5e291d4f20
VictorReMash <158801659+VictorReMash@users.noreply.github.com>
2024-08-27 23:01:26 +0500

    попытка подключиться к удаленной БД

--- examples/connection_db_mssql.py
+++ examples/connection_db_mssql.py
@@ -1,13 +1,13 @@
 import pyodbc
 
 # Настройки подключения
-server = 'server_name_or_ip'  # Имя сервера или IP-адрес
-database = '192.168.3.101'    # Имя базы данных
+server = 'Server2008'  # Имя сервера или IP-адрес
+database = 'faraon_test'    # Имя базы данных
 username = 'МашонкинВР'    # Имя пользователя
 password = '250269'    # Пароль
 
 # Создаем строку подключения
-connection_string = f'DRIVER={{msodbcsql17r}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+connection_string = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
 
 try:
     # Устанавливаем соединение с базой данных
